4be864ee44bbc2fd00b69e7bb099e981c7282151
naoya_t <naoya.tozuka+t@gmail.com>
2012-02-09 22:36:52 +0900

    palindrome化する前のコードだったので差し替え

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,1 @@
-l,i,q;main(n){char b[1];scanf("%d\n",&n);while(n--){gets(b);q=0,i=l=strlen(b)-1;while(--i)q+=b[i]!=b[l-i];puts(q?"NO":"YES");}}
+main(n,l,i,q){char b[2001];scanf("%d\n",&n);while(n--){gets(b);l=strlen(b)-1;for(i=q=0;i<l;++i)if(b[i]-b[l-i])++q;puts(q?"NO":"YES");}}//}};)"SEY":"ON"?q(stup;q++)]i-l[b-]i[b(fi)i++;l<i;0=q=i(rof;1-)b(nelrts=l;)b(steg{)--n(elihw;)n&,"n\d%"(fnacs;]1002[b rahc{)q,i,l,n(niam
